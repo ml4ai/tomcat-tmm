97d8a9ad880cf9025052489b04e1dc5dbfc99f8a
ualiangzhang <liangzh@email.arizona.edu>
2021-03-04 10:52:22 -0700

    add the json file processor and the related files

--- test/read_json.h
+++ test/read_json.h
@@ -5,18 +5,158 @@
 #ifndef TOMCAT_TMM_READ_JSON_H
 #define TOMCAT_TMM_READ_JSON_H
 
+#include "json.hpp"
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
 
+using json = nlohmann::json;
 using namespace std;
 
 class read_json {
   public:
-    read_json(int i) : m_nI(i){}
+    read_json(string diff) : difficulty(diff) {}
     ~read_json() {}
-    void testMemberFunc(int i) const { std::cout << m_nI + i << endl; }
-    int  testResultFun(int i) const { return m_nI + i; }
-    void testBindFun(int num) const { std::cout << m_nI + num << endl; }
-    int m_nI;
+
+    string difficulty;
+
+    json read_json_file() {
+        string file_name;
+
+        json j_file_room;
+        json j_file_portal;
+        json j_file_victim;
+
+        try {
+            file_name = "../../test/data/room_" + this->difficulty + ".json";
+            std::ifstream in_room(file_name);
+            if (!in_room) {
+                std::cout << "Failed to open file" << endl;
+            }
+            j_file_room = json::parse(in_room);
+
+            file_name = "../../test/data/portal_" + this->difficulty + ".json";
+            std::ifstream in_portal(file_name);
+            if (!in_portal) {
+                std::cout << "Failed to open file" << endl;
+            }
+            j_file_portal = json::parse(in_portal);
+
+            file_name = "../../test/data/victim_" + this->difficulty + ".json";
+            std::ifstream in_victim(file_name);
+            if (!in_victim) {
+                std::cout << "Failed to open file" << endl;
+            }
+            j_file_victim = json::parse(in_victim);
+
+        }
+        catch (std::exception& e) {
+            std::cout << "Exception:" << endl;
+            std::cout << e.what() << endl;
+            return 0;
+        }
+
+        // process room data
+        for (int i = 0; i < j_file_room.at("id").size(); i++) {
+            string id = j_file_room.at("id")[i];
+            process_id(id);
+
+            string loc = j_file_room.at("loc")[i];
+            vector<float> locs = process_loc(loc);
+
+            string j_conn = j_file_room.at("connections")[i];
+            vector<string> connections = process_connections(j_conn);
+        }
+
+        // process portal data
+        for (int i = 0; i < j_file_portal.at("id").size(); i++) {
+            string id = j_file_portal.at("id")[i];
+            process_id(id);
+
+            string loc = j_file_portal.at("loc")[i];
+            vector<float> locs = process_loc(loc);
+
+            string j_conn = j_file_portal.at("connections")[i];
+            vector<string> connections = process_connections(j_conn);
+
+        }
+
+        // process victim data
+        for (int i = 0; i < j_file_victim.at("id").size(); i++) {
+            string id = j_file_victim.at("id")[i];
+            process_id(id);
+
+            string loc = j_file_victim.at("loc")[i];
+            vector<float> locs = process_loc(loc);
+
+            string j_type = j_file_victim.at("type")[i];
+            process_id(j_type);
+        }
+
+        return NULL;
+    }
+
+    void process_json() {
+        json j_room, j_portal, j_victim;
+        j_room = read_json_file();
+    }
+
+    vector<float> process_loc(string loc) {
+        loc.erase(std::remove(loc.begin(), loc.end(), '('), loc.end());
+        loc.erase(std::remove(loc.begin(), loc.end(), ')'), loc.end());
+        loc.erase(std::remove(loc.begin(), loc.end(), ','), loc.end());
+        istringstream istr1(loc); // istr1 will read from str
+        float locs[2];
+        istr1 >> locs[0] >> locs[1];
+        vector<float> ret;
+        ret.push_back(locs[0]);
+        ret.push_back(locs[1]);
+        return ret;
+    }
+
+    void* process_id(string id) {
+        id.erase(std::remove(id.begin(), id.end(), '"'), id.end());
+    }
+
+    vector<string> process_connections(string connections) {
+        connections.erase(
+            std::remove(connections.begin(), connections.end(), '"'),
+            connections.end());
+        connections.erase(
+            std::remove(connections.begin(), connections.end(), '['),
+            connections.end());
+        connections.erase(
+            std::remove(connections.begin(), connections.end(), ']'),
+            connections.end());
+        connections.erase(
+            std::remove(connections.begin(), connections.end(), '\''),
+            connections.end());
+        connections.erase(
+            std::remove(connections.begin(), connections.end(), ','),
+            connections.end());
+        vector<string> ret = split(connections, " ");
+        return ret;
+    }
+
+    vector<string> split(const string& str, const string& pattern) {
+        vector<string> ret;
+        if (pattern.empty())
+            return ret;
+        size_t start = 0, index = str.find_first_of(pattern, 0);
+        while (index != str.npos) {
+            if (start != index)
+                ret.push_back(str.substr(start, index - start));
+            start = index + 1;
+            index = str.find_first_of(pattern, start);
+        }
+        if (!str.substr(start).empty())
+            ret.push_back(str.substr(start));
+        return ret;
+    }
 };
 
 #endif // TOMCAT_TMM_READ_JSON_H
--- test/test_graph.cpp
+++ test/test_graph.cpp
@@ -32,8 +32,15 @@
 #include <iostream>          // for std::cout
 #include <utility>           // for std::pair
 #include "read_json.h"
+#include <fstream>
+#include "json.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
 
 using namespace boost;
+using json = nlohmann::json;
+
 
 template <class Graph> struct exercise_vertex {
     exercise_vertex(Graph& g_) : g(g_) {}
@@ -77,93 +84,113 @@ template <class Graph> struct exercise_vertex {
     Graph& g;
 };
 
-int main(int, char*[]) {
-    read_json rj = read_json(10);
-    rj.m_nI = 5;
-    rj.testBindFun(1);
+//int main(int, char*[]) {
+//    read_json rj = read_json(10);
+//    rj.m_nI = 5;
+//    rj.testBindFun(1);
+//
+//    // create a typedef for the Graph type
+//    typedef adjacency_list<vecS,
+//        vecS,
+//        bidirectionalS,
+//        no_property,
+//        property<edge_weight_t, float>>
+//        Graph;
+//
+//    // Make convenient labels for the vertices
+//    enum { A, B, C, D, E, N };
+//    const int num_vertices = N;
+//    const char* name = "ABCDE";
+//
+//    // writing out the edges in the graph
+//    typedef std::pair<int, int> Edge;
+//    Edge edge_array[] = {
+//        Edge(A, B),
+//        Edge(A, D),
+//        Edge(C, A),
+//        Edge(D, C),
+//        Edge(C, E),
+//        Edge(B, D),
+//        Edge(D, E),
+//    };
+//    const int num_edges = sizeof(edge_array) / sizeof(edge_array[0]);
+//
+//    // average transmission delay (in milliseconds) for each connection
+//    float transmission_delay[] = {1.2, 4.5, 2.6, 0.4, 5.2, 1.8, 3.3, 9.1};
+//
+//    // declare a graph object, adding the edges and edge properties
+//#if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
+//    // VC++ can't handle the iterator constructor
+//    Graph g(num_vertices);
+//    property_map<Graph, edge_weight_t>::type weightmap = get(edge_weight, g);
+//    for (std::size_t j = 0; j < num_edges; ++j) {
+//        graph_traits<Graph>::edge_descriptor e;
+//        bool inserted;
+//        tie(e, inserted) =
+//            add_edge(edge_array[j].first, edge_array[j].second, g);
+//        weightmap[e] = transmission_delay[j];
+//    }
+//#else
+//    Graph g(
+//        edge_array, edge_array + num_edges, transmission_delay, num_vertices);
+//#endif
+//
+//    boost::property_map<Graph, vertex_index_t>::type vertex_id =
+//        get(vertex_index, g);
+//    boost::property_map<Graph, edge_weight_t>::type trans_delay =
+//        get(edge_weight, g);
+//
+//    std::cout << "vertices(g) = ";
+//    typedef graph_traits<Graph>::vertex_iterator vertex_iter;
+//    std::pair<vertex_iter, vertex_iter> vp;
+//    for (vp = vertices(g); vp.first != vp.second; ++vp.first)
+//        std::cout << name[get(vertex_id, *vp.first)] << " ";
+//    std::cout << std::endl;
+//
+//    std::cout << "edges(g) = ";
+//    graph_traits<Graph>::edge_iterator ei, ei_end;
+//    for (tie(ei, ei_end) = edges(g); ei != ei_end; ++ei)
+//        std::cout << "(" << name[get(vertex_id, source(*ei, g))] << ","
+//                  << name[get(vertex_id, target(*ei, g))] << ") ";
+//    std::cout << std::endl;
+//
+//    std::for_each(
+//        vertices(g).first, vertices(g).second, exercise_vertex<Graph>(g));
+//
+//    std::map<std::string, std::string> graph_attr, vertex_attr, edge_attr;
+//    graph_attr["size"] = "3,3";
+//    graph_attr["rankdir"] = "LR";
+//    graph_attr["ratio"] = "fill";
+//    vertex_attr["shape"] = "circle";
+//
+//    boost::write_graphviz(
+//        std::cout,
+//        g,
+//        make_label_writer(name),
+//        make_label_writer(trans_delay),
+//        make_graph_attributes_writer(graph_attr, vertex_attr, edge_attr));
+//
+//    return 0;
+//}
 
-    // create a typedef for the Graph type
-    typedef adjacency_list<vecS,
-        vecS,
-        bidirectionalS,
-        no_property,
-        property<edge_weight_t, float>>
-        Graph;
 
-    // Make convenient labels for the vertices
-    enum { A, B, C, D, E, N };
-    const int num_vertices = N;
-    const char* name = "ABCDE";
+int main() {
+    const string difficulty[3] = { "easy", "medium", "hard"};
+    read_json rj = read_json(difficulty[0]);  // select difficulty to generate a graph representation
+    rj.process_json();
 
-    // writing out the edges in the graph
-    typedef std::pair<int, int> Edge;
-    Edge edge_array[] = {
-        Edge(A, B),
-        Edge(A, D),
-        Edge(C, A),
-        Edge(D, C),
-        Edge(C, E),
-        Edge(B, D),
-        Edge(D, E),
-    };
-    const int num_edges = sizeof(edge_array) / sizeof(edge_array[0]);
 
-    // average transmission delay (in milliseconds) for each connection
-    float transmission_delay[] = {1.2, 4.5, 2.6, 0.4, 5.2, 1.8, 3.3, 9.1};
 
-    // declare a graph object, adding the edges and edge properties
-#if defined(BOOST_MSVC) && BOOST_MSVC <= 1300
-    // VC++ can't handle the iterator constructor
-    Graph g(num_vertices);
-    property_map<Graph, edge_weight_t>::type weightmap = get(edge_weight, g);
-    for (std::size_t j = 0; j < num_edges; ++j) {
-        graph_traits<Graph>::edge_descriptor e;
-        bool inserted;
-        tie(e, inserted) =
-            add_edge(edge_array[j].first, edge_array[j].second, g);
-        weightmap[e] = transmission_delay[j];
-    }
-#else
-    Graph g(
-        edge_array, edge_array + num_edges, transmission_delay, num_vertices);
-#endif
+//    rj.set_difficulty(difficulty[0]);
+//    string read_json::difficulty = difficulty[0];
+//    cout << read_json::difficulty <<endl;
+//    cout << rj.difficulty <<endl;
+//    rj.read_json_file();
 
-    boost::property_map<Graph, vertex_index_t>::type vertex_id =
-        get(vertex_index, g);
-    boost::property_map<Graph, edge_weight_t>::type trans_delay =
-        get(edge_weight, g);
+//    rj.m_nI = 5;
+//    rj.testBindFun(1);
+//
+//    json j;
 
-    std::cout << "vertices(g) = ";
-    typedef graph_traits<Graph>::vertex_iterator vertex_iter;
-    std::pair<vertex_iter, vertex_iter> vp;
-    for (vp = vertices(g); vp.first != vp.second; ++vp.first)
-        std::cout << name[get(vertex_id, *vp.first)] << " ";
-    std::cout << std::endl;
-
-    std::cout << "edges(g) = ";
-    graph_traits<Graph>::edge_iterator ei, ei_end;
-    for (tie(ei, ei_end) = edges(g); ei != ei_end; ++ei)
-        std::cout << "(" << name[get(vertex_id, source(*ei, g))] << ","
-                  << name[get(vertex_id, target(*ei, g))] << ") ";
-    std::cout << std::endl;
-
-    std::for_each(
-        vertices(g).first, vertices(g).second, exercise_vertex<Graph>(g));
-
-    std::map<std::string, std::string> graph_attr, vertex_attr, edge_attr;
-    graph_attr["size"] = "3,3";
-    graph_attr["rankdir"] = "LR";
-    graph_attr["ratio"] = "fill";
-    vertex_attr["shape"] = "circle";
-
-    boost::write_graphviz(
-        std::cout,
-        g,
-        make_label_writer(name),
-        make_label_writer(trans_delay),
-        make_graph_attributes_writer(graph_attr, vertex_attr, edge_attr));
-
-    return 0;
 }
-
 
