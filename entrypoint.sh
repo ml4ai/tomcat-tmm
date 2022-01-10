061980522239750a451c115c3043b7f2d93eea1d
Paulo Soares <paulosoares@SBS-7186.local>
2022-01-10 10:38:40 -0300

    Updating entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,3 @@
-./build/bin/start_agent --model_dir "data/study3_spiral1/models/" --broker_json "data/study3_spiral1/online/broker.json" --agent_json "data/study3_spiral1/agents/team_quality_online_agent.json" --seconds 900 --step_size 1 --particles 1000 --jobs 10 --study_num 3
+./build/bin/start_agent --model_dir "data/study3_spiral3/models/" --broker_json "data/study3_spiral3/online/broker.json" --agent_json "data/study3_spiral3/agents/team_quality_online_agent.json" --params_dir "../../data/asist/study3_spiral3/parameters" --map_json "../../data/asist/study3_spiral3/maps/Saturn_2.1_3D_sm_v1.0.json" --seconds 600 --step_size 1 --particles 2000 --jobs 10 --study_num 3
+
+
