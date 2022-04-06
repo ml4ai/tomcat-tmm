560f64ef43a4a701d05d8c4dd14729a627320c33
Paulo Soares <paulosoares@SBS-7186.local>
2022-04-06 10:36:19 -0700

    Updating docker to store logs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-./build/bin/start_agent --model_dir "data/asist/study3_intervention1/model" --params_dir "data/asist/study3_intervention1/parameter" --agent_json "data/asist/study3_intervention1/agent/agent_settings.json" --broker_json "data/asist/study3_intervention1/online/broker_settings.json" --reporter "asist_study3_reporter" --reporter_settings_json "data/asist/study3_intervention1/reporter/reporter_settings.json" --seconds=900 --jobs=1 --players=3 --study_num=3
+./build/bin/start_agent --model_dir "data/asist/study3_intervention1/model" --params_dir "data/asist/study3_intervention1/parameter" --agent_json "data/asist/study3_intervention1/agent/agent_settings.json" --broker_json "data/asist/study3_intervention1/online/broker_settings.json" --reporter "asist_study3_reporter" --reporter_settings_json "data/asist/study3_intervention1/reporter/reporter_settings.json" --log_dir "data/asist/study3_intervention1/logs" --seconds=900 --jobs=1 --players=3 --study_num=3
 
 
